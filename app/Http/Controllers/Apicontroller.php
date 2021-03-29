73f3f592a319381b7f5719c0c3864787c81c46b6
banyuaji <nyuudesu2@gmail.com>
2021-03-29 19:05:18 +0700

    api kategori

---
+++ app/Http/Controllers/Apicontroller.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Kategori;
+
+class Apicontroller extends Controller
+{
+    // kategori
+    public function index()
+    {
+        return Kategori::all();
+    }
+    public function create(Request $req)
+    {
+        $kat = new Kategori;
+        $kat->nama_kategori = $req->nama;
+        $kat->save();
+
+        return "Data berhasil masuk";
+    }
+    public function update(Request $req, $id)
+    {
+        $nama = $req->nama;
+
+        $kat = Kategori::find($id);
+        $kat->nama_kategori = $nama;
+        $kat->save();
+
+        return "Data berhasil diupdate";
+    }
+    public function delete($id)
+    {
+        $kat = Kategori::find($id);
+        $kat->delete();
+
+        return "Data berhasil dihapus";
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Apicontroller;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/kategori', [Apicontroller::class, 'index']);
+Route::post('/kategori/create', [Apicontroller::class, 'create']);
+Route::put('/kategori/update/{id}', [Apicontroller::class, 'update']);
+Route::delete('/kategori/delete/{id}', [Apicontroller::class, 'delete']);
